970ca4b857400ccded46ce15553bbaa4dabdc73c
ZMAN <etta1055@gmail.com>
2021-06-01 12:45:47 -0500

    adding dice roll to current players score

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +15,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
     //1. generating a random dice roll
@@ -20,6 +24,15 @@ btnRoll.addEventListener('click', function() {
 
     //2. display dice
     diceEl.classList.remove('hidden');
-
+    //using template literal to display dice block 
+    diceEl.src = `dice-${dice}.png`;
+    
     //3. check for rolled 1: if true, switch to next player
-})
+    if(dice !== 1) {
+        //Add dice to current score if not 1
+        currentScore = currentScore + dice;
+        current0El.textContent = currentScore; //  CHANGE LATER
+    } else {
+        //Swich to next palyer
+    }
+});
