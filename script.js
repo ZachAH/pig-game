caca21a15478140a79633894ca5fb0bec701eaa1
ZMAN <etta1055@gmail.com>
2021-06-01 13:02:03 -0500

    toggling player--active for players

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -31,8 +35,14 @@ btnRoll.addEventListener('click', function() {
     if(dice !== 1) {
         //Add dice to current score if not 1
         currentScore = currentScore + dice;
-        current0El.textContent = currentScore; //  CHANGE LATER
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
-        //Swich to next palyer
+        //Swich to next palyer if the active player is 0 then switch to player 1 but if player is one switch to player zero then set score to zero
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 });
+        
